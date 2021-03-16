f36438880bfafa5e9e96bb3f9c221afce7f7282b
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-03-16 20:35:58 +0530

    Add files via upload

---
+++ LL1.java
@@ -0,0 +1,48 @@
+class LL1
+{
+  public static class Node
+  {
+    int data;
+    Node next;
+  }
+  public   static class LinkedList
+  {
+     Node head,tail;
+     int size;
+        public  void addLast(int data)
+        {
+         Node node=new Node();
+         node.data=data;
+            if(size==0)
+            {
+              head=tail=node;
+            }
+            else
+            {
+               tail.next=node;
+               tail=node;
+            }
+            size++;
+        }
+
+        public void display()
+        {
+           Node temp=head;
+           while(temp!=null)
+           {
+              System.out.println(" "+temp.data);
+              temp=temp.next;
+           }
+              System.out.println(" .");
+
+        }
+  }
+
+    public static void main(String aa[])
+    {
+      LinkedList l1=new LinkedList();
+      l1.addLast(10);
+      l1.addLast(20);
+      l1.display();
+    }
+}
