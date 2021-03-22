1ee3522cf341993757001ee93f3c5a75e85966cf
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2021-03-22 22:21:24 +0530

    Create msp.java

---
+++ msp.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class map1
+{
+  public static void main(String aa[])
+  {
+     Map<Integer,String> m1=new HashMap<Integer,String>();
+     m1.put(1,"Saurabh");
+     System.out.println(m1);
+  }
+}
